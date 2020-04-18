b9dddd5e7e33a27c5a1d0a88e0b6ff1050484668
vivianm94 <vivianmascarenhas1994@gmail.com>
2020-04-18 18:15:38 +0530

    sec commit

---
+++ ConsoleApplication1/ConsoleApplication1/JsonReadWrite.cs
@@ -0,0 +1,96 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Product
+    {
+        public string url { get; set; }
+        public string value { get; set; }
+    }
+
+    public class RootObject
+    {
+        public List<Product> product { get; set; }
+    }
+    class JsonReadWrite
+    {
+        public void GenerateJson(int numProduct)
+        {
+            List<Product> listProduct = new List<Product>();
+            for (int i=0;i<numProduct;i++)
+            {
+                Console.WriteLine("Enter the product url and min price.");
+                string productUrl = Console.ReadLine();
+                string productMin = Console.ReadLine();
+                Product p1 = new Product();
+                p1.url = productUrl;
+                p1.value = productMin;
+                listProduct.Add(p1);
+            }
+
+            RootObject rt = new RootObject();
+            rt.product = listProduct;
+            string strJson = JsonConvert.SerializeObject(rt);  
+            System.IO.File.WriteAllText(@"H:\TrackAmazonProduct-master\TrackAmazonProduct-master\ConsoleApplication1\ConsoleApplication1\product.json", strJson);      
+        }    
+
+        //public void readFile()
+        //{
+        //    string Json = File.ReadAllText(@"H:\TrackAmazonProduct-master\TrackAmazonProduct-master\ConsoleApplication1\ConsoleApplication1\json1.json");
+        //    var dict = JsonConvert.DeserializeObject<IDictionary>(Json);
+
+        //    foreach (var newdic in dict)
+        //    {
+        //        var newd = newdic;
+        //        var key = ((System.Collections.DictionaryEntry)newd).Key;
+
+        //        if (key.Equals("medications"))
+        //        {
+        //            var newdn = ((System.Collections.DictionaryEntry)newd).Value;
+        //            JArray Jary = JArray.Parse(newdn.ToString());
+        //            foreach (JObject newdic12 in Jary)
+        //            {
+        //                var ne = newdic12;
+
+        //                foreach (var rea in ne.Children())
+        //                {
+        //                    var newrea = rea;
+
+        //                    if (((Newtonsoft.Json.Linq.JProperty)newrea).Name.Equals("betaBlocker"))
+        //                    {
+        //                        var newwe = newrea.Values().Children().ToArray();
+
+        //                        foreach (var newa12 in newwe)
+        //                        {
+        //                            var property = ((Newtonsoft.Json.Linq.JProperty)(newa12)).Name;
+
+        //                            if(property.Equals("dose"))
+        //                            {
+        //                                ((Newtonsoft.Json.Linq.JProperty)newa12).Value = "asdf";
+        //                            }
+        //                        }
+                               
+        //                    }
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+
+
+        public object readFile()
+        {
+            string jsonStr = File.ReadAllText(@"H:\TrackAmazonProduct-master\TrackAmazonProduct-master\ConsoleApplication1\ConsoleApplication1\product.json");
+            RootObject objJson= JsonConvert.DeserializeObject<RootObject>(jsonStr);
+            return objJson;
+        }
+    }
+}
--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -15,48 +17,81 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            // string url = "https://www.amazon.in/GENERIC-Ultra-Mini-Bluetooth-Dongle-Adapter/dp/B0117H7GZ6/ref=sr_1_14?crid=36FN332QCAZBD&dchild=1&keywords=earphones+under+200+rupees&qid=1586759890&sprefix=earp%2Caps%2C346&sr=8-14";
-
-            string url = "https://www.amazon.in/iVoltaa-Micro-USB-Type-Adapter/dp/B075F927V2/ref=lp_1389401031_1_4?s=electronics&ie=UTF8&qid=1586778682&sr=1-4";
-
-            HttpClient httpClient = new HttpClient();
-
-            var html = httpClient.GetStringAsync(url);
-
-            // Console.WriteLine(html.Result);
-
-            HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
-            htmlDocument.LoadHtml(html.Result.ToString().Trim());
-
-            var nodeProduct = htmlDocument.DocumentNode.SelectNodes("//span[@id='productTitle']");
-
-            var nodePrice= htmlDocument.DocumentNode.SelectNodes("//span[@id='priceblock_ourprice']");
-
-            string ProductName = nodeProduct[0].InnerText.ToString().Trim();
-
-            var ProductPrice = nodePrice[0].InnerText.ToString().Trim().Replace(" ", "");
-
-            var newProductPrice = Decimal.Parse(Regex.Replace(ProductPrice, @"\s+", String.Empty), NumberStyles.Currency);
-
-            Console.WriteLine("ProductName:"+ ProductName+" Price: "+ newProductPrice);
-
-            if (newProductPrice<179)
+            JsonReadWrite st = new JsonReadWrite();
+            if (File.Exists(@"H:\TrackAmazonProduct-master\TrackAmazonProduct-master\ConsoleApplication1\ConsoleApplication1\product.json"))
             {
-                sendMail(ProductName, newProductPrice);
+                Console.WriteLine("Product file exist do you want to create new product File? [y/n]");
+                ConsoleKeyInfo cki = Console.ReadKey();
+
+                if (cki.Key.ToString().ToLower().Equals("y"))
+                {
+                    createNewJson(st);
+                }
+            }
+            else
+            {
+                createNewJson(st);
+            }
+  
+            object objJson=st.readFile();
+            int productCount = ((ConsoleApplication1.RootObject)objJson).product.Count;
+            string[] productName = new string[productCount];
+            string[] productValue = new string[productCount];
+
+            bool sendMail = false;
+
+            for (int k=0;k<productCount; k++)
+            {
+                // string url = "https://www.amazon.in/GENERIC-Ultra-Mini-Bluetooth-Dongle-Adapter/dp/B0117H7GZ6/ref=sr_1_14?crid=36FN332QCAZBD&dchild=1&keywords=earphones+under+200+rupees&qid=1586759890&sprefix=earp%2Caps%2C346&sr=8-14";
+
+                //string url = "https://www.amazon.in/iVoltaa-Micro-USB-Type-Adapter/dp/B075F927V2/ref=lp_1389401031_1_4?s=electronics&ie=UTF8&qid=1586778682&sr=1-4";
+
+                string url = ((ConsoleApplication1.RootObject)objJson).product[k].url;
+
+                HttpClient httpClient = new HttpClient();
+
+                Task<string> html = httpClient.GetStringAsync(url);
+
+                HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
+                htmlDocument.LoadHtml(html.Result.ToString().Trim());
+
+                string nodeProduct = htmlDocument.DocumentNode.SelectNodes("//span[@id='productTitle']")[0].InnerText.ToString().Trim();
+
+                decimal nodePrice = Decimal.Parse(Regex.Replace(htmlDocument.DocumentNode.SelectNodes("//span[@id='priceblock_ourprice']")[0].InnerText.ToString().Trim().Replace(" ", ""), @"\s+", String.Empty), NumberStyles.Currency);
+
+                //Console.WriteLine("ProductName:" + nodeProduct + " Price: " + nodePrice);
+
+                if (nodePrice <= int.Parse(((ConsoleApplication1.RootObject)objJson).product[k].value))
+                {
+                    sendMail = true;
+                    productName[k] = nodeProduct;
+                    productValue[k] = nodePrice.ToString();
+                }
+
             }
 
+            if (sendMail==true)
+            {
+               SendMail(String.Join(", ", productName),String.Join(", ", productValue));
+            }
         }
 
-
-        public static void sendMail(string ProductName,decimal newProductPrice)
+        public static void createNewJson(JsonReadWrite st)
+        {
+            Console.WriteLine("\nEnter the number of product you want to track");
+            string numProduct = Console.ReadLine();       
+            st.GenerateJson(int.Parse(numProduct));
+        }
+        public static void SendMail(string productName,string newProductPrice)
         {
             try
             {
-                var fromAddress = new MailAddress("emailaddress", "From Name");
-                var toAddress = new MailAddress("emailaddress", "To Name");
+                string body = "The Price of Products " + productName + " is " + newProductPrice;
+                MailAddress fromAddress = new MailAddress("emailaddress", "From Name");
+                MailAddress toAddress = new MailAddress("emailaddress", "To Name");
                 const string fromPassword = "emailpassword";
                 const string subject = "Price Has Been Decreased of Product";
-                string body = "The Price of Product" + ProductName + "is" + newProductPrice;
+                
 
                 var smtp = new SmtpClient
                 {
@@ -77,7 +112,7 @@ namespace ConsoleApplication1
                 }
 
 
-                Console.WriteLine("mail Send");
+                Console.WriteLine("Mail has been send");
             }
             catch (Exception ex)
             {             
